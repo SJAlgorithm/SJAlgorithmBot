a7c385863dac51dcc433dc5b9655a01ede96124b
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-12-20 19:33:50 +0900

    update with GithubApi&random number

--- SJAlgorithmBot/src/main/java/SJAlgorithmBot.java
+++ SJAlgorithmBot/src/main/java/SJAlgorithmBot.java
@@ -1,5 +1,9 @@
 public class SJAlgorithmBot {
+
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        String token=args[0];
+        GitAPI gitAPI = new GitAPI(token);
+        gitAPI.readContents("SJAlgorithm","SJAlgorithmBot","README.md");
+        gitAPI.updateContents("SJAlgorithm","SJAlgorithmBot","README.md");
     }
 }
